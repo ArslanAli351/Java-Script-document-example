fcce10ff1f009731a1ce4f55ec054fce24632beb
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-02 22:31:22 -0800

    Java script

---
+++ Javascript/app.js
@@ -0,0 +1,7 @@
+
+alert("Welcom to webpage")
+document.write("<h1 >Hello <b>Java Script</b> Planet</h1>");
+document.write("<h3>I am the <span>master</span> of my own fate.</h3>")
+document.write("<h3>I am the <span>champoin</span> of my own soule.</h3>")
+document.write("<h3>I am the <span>superhero</span> of my own life.</h3>")
+
